755ebae76ebab6a8fbbeebadb4488686d28544ff
Lucas Sedano <114513775+LucasSedano-ewu@users.noreply.github.com>
2022-10-11 11:46:14 -0700

    Curved Text
    * Connect JS Library
    * Code from CodePen
    * Boiler from Github and Netlify

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 // My Scripts
 
 console.log('Page Loaded');
+
+newCircle = function() {
+    var circle = document.createElement('div');
+    circle.className = 'circle';
+    document.body.appendChild(circle);
+    return circle;
+}
