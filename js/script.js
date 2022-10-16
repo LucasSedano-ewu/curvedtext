4aeaf8158074f16fb7d3d0dd3bc34dd1edb1caa1
Lucas Sedano <114513775+LucasSedano-ewu@users.noreply.github.com>
2022-10-16 16:37:11 -0700

    Fixed linking to scripts and Comico font

--- js/script.js
+++ js/script.js
@@ -1,13 +1,6 @@
 // My Scripts
 console.log('Page Loaded');
 
-newCircle = function() {
-    var circle = document.createElement('div');
-    circle.className = 'circle';
-    document.body.appendChild(circle);
-    return circle;
-}
-
-// <h1 id="demo1">Decisions define the Direction of the Design. Design without Direction -- is just Decoration. Anyone who makes Decisions is a: Designer. </h1></h2>
 new CircleType(document.getElementById('demo1'))
-  .radius(384);
+  .radius(384)
+  .dir(-1);
